6093e6772214549900f339b2ff07932cc81dc2be
seangah8 <seangah8@gmail.com>
2024-12-11 15:27:30 +0200

    design AppHeader

--- pages/AppHeader.jsx
+++ pages/AppHeader.jsx
@@ -5,6 +5,7 @@ export function AppHeader() {
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
+                <img src="./assets/BooksImages/missbooks-icon.png"/>
                 <nav className="app-nav">
                     <NavLink to="/home">Home</NavLink>
                     <NavLink to="/book">Books</NavLink>
