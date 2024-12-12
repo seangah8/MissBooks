39cc2ecb6e0e32e4d9eb3043f9069f37e13ae999
seangah8 <seangah8@gmail.com>
2024-12-12 16:38:31 +0200

    desigh footer component

--- pages/AppFooter.jsx
+++ pages/AppFooter.jsx
@@ -3,6 +3,18 @@ export function AppFooter() {
 
     return (
         <footer className="app-footer">
+            <div className="text">
+                <h3>MissBooks</h3>
+                <p>© MissBooks International Ltd. 2024</p>
+            </div>
+
+            <div className="icons">
+                <i className="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-tiktok"></i>
+                <i class="fa-brands fa-linkedin"></i>
+            </div>
         </footer>
     )
 }
