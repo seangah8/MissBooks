4c273b8ddf7793ef4f6b71fe4943f67fd1e2bb18
seangah8 <seangah8@gmail.com>
2024-12-12 06:16:21 +0200

    redesigh the AboutUs component

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -1,49 +1,57 @@
-
-const { useState } = React
-
 export function AboutUs(){
-
-    const [msgOpen,setMsgOpen] = useState(false)
-
-    function onOpenToggel(){
-        setMsgOpen(pev => !pev)
-    }
-
 
     return(
         <section className="about-us">
+            <section className="about-us-grid">
 
-            <div 
-            className={msgOpen?"msg-background open":"msg-background"}
-            onClick={onOpenToggel}
-            >
-                <h1 className={msgOpen?"title open":"title"}>
-                    {msgOpen?"About Us":"About Us?"}</h1>
+                <h1 className="title-1">
+                    <div>So how did</div>
+                    <span>MissBooks</span>
+                    <div>come to be?</div>
+                </h1>
 
-                {
-                msgOpen?<p>Welcome to MissBooks, where stories come to life
-                and readers find their perfect match!
-                Founded by a team of passionate bibliophiles,
-                MissBooks was created to bring the joy of
-                reading to everyone, everywhere. Our company
-                prides itself on offering a vast and thoughtfully
-                curated selection of books, from beloved classics
-                to the latest bestsellers, ensuring theres
-                something for every taste and age. Were more than
-                just a bookstore—we’re a community dedicated to
-                celebrating the magic of reading and the authors
-                who create it. At MissBooks, we combine our love for
-                literature with a commitment to exceptional customer
-                service, making your shopping experience as enjoyable
-                as the stories you’ll discover.
-                Join us in our mission to connect the world
-                through the power of books. Let’s turn pages,
-                spark imaginations, and share the love of reading
-                together!</p> : ''
-                }
-            </div>
-            
-            
+                <p className="paragraph-1">Well for us, it happened somewhere in between
+                collaborating and communicating, engaging,
+                and scaling rapidly. All while being totally
+                transparent and working the way we want.</p>
+                
+                <img className="image-1" 
+                src="https://basecamplive.com/wp-content/uploads/2023/02/books.jpeg"/>
+                
+                <h1 className="title-2">It's all about the books</h1>
+
+                <p className="paragraph-2">As we integrated and
+                automated, built workflows, created templates,
+                and expanded our community beyond our wildest
+                expectations, we continued to grow.
+                It is through this journey that the MissBooks
+                Work OS was born.</p>
+
+                <div className="images">
+                    <img className="image-2"
+                    src="https://png.pngtree.com/thumb_back/fw800/background/20230901/pngtree-a-book-next-to-cup-of-coffee-with-flowers-image_13175126.jpg"
+                    />
+
+                    <img className="image-3"
+                    src="https://miro.medium.com/v2/resize:fit:1400/0*4WPgySDfHBSQHsmD"
+                    />
+                </div>
+
+                <img className="image-4" 
+                src="https://images.stockcake.com/public/d/a/d/dad39ecd-71ee-4b22-bb48-67798c0272a4_large/reading-in-library-stockcake.jpg"/>
+
+                <p className="paragraph-3">
+                    June 10th, 2021 marked the start of a new era
+                    for MissBooks we rang the opening bell and
+                    officially became a publicly traded company
+                    on Nasdaq.<br/><br/> These days, we continue to fuel our
+                    growth by evolving into a multi-product
+                    company, providing people, teams, and
+                    companies powerful products to help turn
+                    their work visions into a reality.
+                    <span> We’re only just getting started.</span></p>
+
+            </section>
         </section>
         
     )
